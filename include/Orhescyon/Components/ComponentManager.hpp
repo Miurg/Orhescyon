43c6ea5e13d9658be627efd38cf59baea6d18558
Shimu <rashnimorgen@gmail.com>
2026-03-10 09:54:57 +0100

    feat(component): remove static things

--- include/Orhescyon/Components/ComponentManager.hpp
+++ include/Orhescyon/Components/ComponentManager.hpp
@@ -9,28 +9,28 @@
 #include "ComponentArray.hpp"
 namespace Orhescyon
 {
-// Type-erased component storage. Each component type gets a static ComponentArray.
+// Type-erased component storage. Each component type gets a ComponentArray.
 // removeEntity() uses registered callbacks to erase from all arrays without knowing concrete types.
 class ComponentManager
 {
 private:
+	std::unordered_map<std::type_index, std::shared_ptr<void>> _componentArrays;
+	std::unordered_map<std::type_index, std::function<void(Entity)>> _removeCallbacks;
+
 	template <typename TComponent>
-	static ComponentArray<TComponent>& getComponentArray()
+	ComponentArray<TComponent>& getComponentArray()
 	{
-		struct RegisteredArray : public ComponentArray<TComponent>
+		auto typeIndex = std::type_index(typeid(TComponent));
+		if (_componentArrays.find(typeIndex) == _componentArrays.end())
 		{
-			RegisteredArray()
+			_componentArrays[typeIndex] = std::make_shared<ComponentArray<TComponent>>();
+			_removeCallbacks[typeIndex] = [this](Entity entity)
 			{
-				ComponentManager::_removeCallbacks[std::type_index(typeid(TComponent))] = [](Entity entity)
-				{ getComponentArray<TComponent>().removeComponent(entity); };
-			}
-		};
-
-		static RegisteredArray array;
-		return array;
+				getComponentArray<TComponent>().removeComponent(entity);
+			};
+		}
+		return *std::static_pointer_cast<ComponentArray<TComponent>>(_componentArrays[typeIndex]);
 	}
-
-	inline static std::unordered_map<std::type_index, std::function<void(Entity)>> _removeCallbacks;
 
 public:
 	template <typename TComponent, typename... Args>
