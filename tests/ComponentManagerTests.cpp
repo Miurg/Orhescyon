6b155f0cffcc8abd6a0ad2bf19296d127bbe18ec
Shimu <rashnimorgen@gmail.com>
2026-03-10 10:23:33 +0100

    feat(tests): add component manager tests

---
+++ tests/ComponentManagerTests.cpp
@@ -0,0 +1,111 @@
+#include <Orhescyon/Components/ComponentManager.hpp>
+#include <gtest/gtest.h>
+
+
+using namespace Orhescyon;
+
+struct Position 
+{
+	float x, y;
+};
+struct Health 
+{
+	int value;
+};
+struct Name 
+{
+	std::string text;
+	int id;
+	Name(std::string t, int i) : text(std::move(t)), id(i) {}
+};
+
+TEST(ComponentManager, AddAndGetComponent) 
+{
+	ComponentManager cm;
+	Entity e1 = 1;
+
+	Position* p = cm.addComponent<Position>(e1, Position{ 1.0f, 2.0f });
+	ASSERT_NE(p, nullptr);
+	EXPECT_FLOAT_EQ(p->x, 1.0f);
+	EXPECT_FLOAT_EQ(p->y, 2.0f);
+
+	Position* p2 = cm.getComponent<Position>(e1);
+	ASSERT_EQ(p, p2);
+}
+
+TEST(ComponentManager, RemoveEntityClearsAllItsComponents) 
+{
+	ComponentManager cm;
+	Entity e1 = 1;
+	Entity e2 = 2;
+
+	cm.addComponent<Position>(e1, Position{ 1.0f, 2.0f });
+	cm.addComponent<Health>(e1, Health{ 100 });
+	cm.addComponent<Position>(e2, Position{ 3.0f, 4.0f });
+
+	cm.removeEntity(e1);
+
+	// Entity 1 components should be gone
+	EXPECT_EQ(cm.getComponent<Position>(e1), nullptr);
+	EXPECT_EQ(cm.getComponent<Health>(e1), nullptr);
+
+	// Entity 2 components should remain untouched
+	Position* p2 = cm.getComponent<Position>(e2);
+	ASSERT_NE(p2, nullptr);
+	EXPECT_FLOAT_EQ(p2->x, 3.0f);
+}
+
+TEST(ComponentManager, GetAllComponentsReturnsCorrectArray) 
+{
+	ComponentManager cm;
+	Entity e1 = 1;
+	Entity e2 = 2;
+
+	cm.addComponent<Position>(e1, Position{ 1.0f, 2.0f });
+	cm.addComponent<Position>(e2, Position{ 3.0f, 4.0f });
+
+	ComponentArray<Position>& positions = cm.getAllComponents<Position>();
+	EXPECT_EQ(positions.size(), 2u);
+
+	// Check we can retrieve components from the returned array
+	Position* p1 = positions.getComponent(e1);
+	ASSERT_NE(p1, nullptr);
+	EXPECT_FLOAT_EQ(p1->x, 1.0f);
+}
+
+TEST(ComponentManager, PerfectForwardingArguments) 
+{
+	ComponentManager cm;
+	Entity e1 = 1;
+
+	Name* n = cm.addComponent<Name>(e1, "TestEntity", 42);
+
+	ASSERT_NE(n, nullptr);
+	EXPECT_EQ(n->text, "TestEntity");
+	EXPECT_EQ(n->id, 42);
+}
+
+TEST(ComponentManager, LazyInitializationDoesNotThrow) 
+{
+	ComponentManager cm;
+	Entity e1 = 1;
+
+	EXPECT_EQ(cm.getComponent<Position>(e1), nullptr);
+
+	EXPECT_NO_THROW(cm.removeComponent<Position>(e1));
+
+	// Getting the array should return an empty array
+	ComponentArray<Position>& positions = cm.getAllComponents<Position>();
+	EXPECT_EQ(positions.size(), 0u);
+}
+
+TEST(ComponentManager, TypeIsolation) 
+{
+	ComponentManager cm;
+	Entity e1 = 1;
+
+	cm.addComponent<Position>(e1, Position{ 1.0f, 2.0f });
+
+	// Adding Position should not mysteriously add Health
+	EXPECT_EQ(cm.getComponent<Health>(e1), nullptr);
+}
